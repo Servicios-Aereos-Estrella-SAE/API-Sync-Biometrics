ffcd91ad69661706d001971a96cc806103677c53
Rogelio Rafael Jinestas García <roger_17@live.com>
2024-05-29 11:31:24 -0600

    style(RHATM0100 General): 0RHATM0100M001001 Conectar con PostgresSQL
    Se corrigieron nombres del .env y se eliminaron variables no utilizadas en iclock_transaction_controller

--- app/controllers/iclock_transactions_controller.ts
+++ app/controllers/iclock_transactions_controller.ts
@@ -76,7 +76,7 @@ export default class IClockTransactionsController {
    *                     type: number
    */
   @inject()
-  index(ctx: HttpContext, i_clock_terminal_services: IClockTransactionService) {
+  index(i_clock_terminal_services: IClockTransactionService) {
     // return i_clock_terminal_services.getAllTransactions()
     // return response all transactions json
     return i_clock_terminal_services.getAllTransactions()
